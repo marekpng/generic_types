2b95748c8bfd5b12e60eac4130c88a485dd933f5
Marek <marekjarko90@gmail.com>
2023-02-19 17:47:29 +0100

    generika

---
+++ src/main.rs
@@ -0,0 +1,109 @@
+// fn largest<T>(list: &[T]) -> &T {
+
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let char_list = vec!['y','m','a'];
+//     let result_char = largest(&char_list
+//     let result = largest(&number_list);
+    
+
+    
+//     println!("The largest char is {}", result_char);
+//     assert_eq!(*result, 6000);
+// }
+
+
+//===================================================================================
+// pub trait Summary {
+//     fn summarize(&self) -> String {
+//         String::from("(Read more...)")
+//     }
+// }
+
+// pub struct NewsArticle {
+//     pub headline: String,
+//     pub location: String,
+//     pub author: String,
+//     pub content: String,
+// }
+
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
+
+// pub struct Tweet {
+//     pub username: String,
+//     pub content: String,
+//     pub reply: bool,
+//     pub retweet: bool,
+// }
+
+// impl Summary for Tweet {
+//     fn summarize(&self) -> String {
+//         format!("{}: {}", self.username, self.content)
+//     }
+// }
+// // use aggregator::{Summary, Tweet};
+
+// fn main() {
+//     let tweet = Tweet {
+//         username: String::from("horse_ebooks"),
+//         content: String::from(
+//             "of course, as you probably already know, people",
+//         ),
+//         reply: false,
+//         retweet: false,
+//     };
+
+//     println!("1 new tweet: {}", tweet.summarize());
+
+
+//     let article = NewsArticle {
+//         headline: String::from("Penguins win the Stanley Cup Championship!"),
+//         location: String::from("Pittsburgh, PA, USA"),
+//         author: String::from("Iceburgh"),
+//         content: String::from(
+//             "The Pittsburgh Penguins once again are the best \
+//              hockey team in the NHL.",
+//         ),
+//     };
+
+//     println!("New article available! {}", article.summarize());
+// }
+//===========================================================================
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
